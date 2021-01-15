10185718e839d492322fc75211005b30355faeae
qj-mayunfei <35836208@qq.com>
2021-01-15 22:29:29 +0800

    test by cm

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  #test
+  #test edited by cm
+  #2021-01-15
 end
